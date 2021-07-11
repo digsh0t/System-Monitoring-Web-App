5f2fb39c856be28df66710935fc92281a59a909c
Tri Le <wintltr@gmail.com>
2021-07-11 16:53:24 +0700

    Test main.go

--- go.mod
+++ go.mod
@@ -8,4 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/pkg/sftp v1.13.1 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/wintltr/login-api/routes"
+	sshConnect "github.com/wintltr/login-api/ssh_connect"
 )
 
 func main() {
+	sshConnect.ConnectWithPassword("dscmember", "dsc@2021", "192.168.163.136", "22")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/login", routes.Login).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", router))
