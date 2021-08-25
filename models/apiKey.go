e99c61f380b778d4381508baf1aaafdf96dea9a1
Tri Le <wintltr@gmail.com>
2021-08-25 15:13:03 +0700

    add register for alert and send alert function

--- models/apiKey.go
+++ models/apiKey.go
@@ -3,6 +3,7 @@ package models
 import (
 	"errors"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/wintltr/login-api/database"
 )
 
@@ -68,5 +69,56 @@ func RemoveTelegramAPIKeyFromDB(apiName string) error {
 	if rows == 0 {
 		return errors.New("no API Keys with this name exist")
 	}
+	return err
+}
+
+func RegisterForAlertTelegram() (int64, error) {
+	telegramApiToken, err := GetTelegramToken()
+	if err != nil {
+		return -1, err
+	}
+	bot, err := tgbotapi.NewBotAPI(telegramApiToken)
+	if err != nil {
+		return -1, err
+	}
+
+	bot.Debug = true
+
+	updateConfig := tgbotapi.NewUpdate(0)
+
+	// Tell Telegram we should wait up to 30 seconds on each request for an
+	// update. This way we can get information just as quickly as making many
+	// frequent requests without having to send nearly as many.
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates, _ := bot.GetUpdates(updateConfig)
+
+	// Let's go through each update that we're getting from Telegram.
+	for _, update := range updates {
+		// Telegram can send many types of updates depending on what your Bot
+		// is up to. We only want to look at messages for now, so we can
+		// discard any other updates.
+		if update.Message == nil {
+			continue
+		}
+
+		return update.Message.Chat.ID, err
+	}
+	return -1, err
+}
+
+func SendTelegramMessage(chatId int64, message string) error {
+	telegramApiToken, err := GetTelegramToken()
+	if err != nil {
+		return err
+	}
+	bot, err := tgbotapi.NewBotAPI(telegramApiToken)
+	if err != nil {
+		return err
+	}
+
+	msg := tgbotapi.NewMessage(chatId, message)
+	_, err = bot.Send(msg)
 	return err
 }
