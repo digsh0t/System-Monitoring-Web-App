89ec9082626e85fb2f33689d7784a1a579a10ef6
longhk2802 <longhk2802@gmail.com>
2021-08-29 21:51:07 -0700

    add pc list api

--- routes/packageListAll.go
+++ routes/packageListAll.go
@@ -7,34 +7,55 @@ import (
 
 	"github.com/bitly/go-simplejson"
 	"github.com/gorilla/mux"
+	"github.com/wintltr/login-api/auth"
 	"github.com/wintltr/login-api/models"
 	"github.com/wintltr/login-api/utils"
 )
 
 func PackageListAll(w http.ResponseWriter, r *http.Request) {
+	var (
+		PackageList []models.PackageInstalledInfo
+		err         error
+	)
 
-	/*
-		isAuthorized, err := auth.CheckAuth(r, []string{"admin", "user"})
-		if err != nil {
-			utils.ERROR(w, http.StatusUnauthorized, errors.New("invalid token").Error())
-			return
-		}
-		if !isAuthorized {
-			utils.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized").Error())
-			return
-		}
-	*/
-	returnJson := simplejson.New()
-	vars := mux.Vars(r)
-	sshConnectionId, err := strconv.Atoi(vars["id"])
+	//Authorization
+	isAuthorized, err := auth.CheckAuth(r, []string{"admin", "user"})
 	if err != nil {
-		returnJson.Set("Status", false)
-		returnJson.Set("Error", errors.New("invalid SSH Connection id").Error())
-		utils.JSON(w, http.StatusBadRequest, returnJson)
+		utils.ERROR(w, http.StatusUnauthorized, errors.New("invalid token").Error())
+		return
+	}
+	if !isAuthorized {
+		utils.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized").Error())
 		return
 	}
 
-	PackageList, err := models.GetAllPackageFromHostID(sshConnectionId)
+	returnJson := simplejson.New()
+	vars := mux.Vars(r)
+	stringId := vars["id"]
+	if stringId == "all" {
+		PackageList, err = models.GetAllPackage()
+		if err != nil {
+			returnJson.Set("Status", false)
+			returnJson.Set("Error", errors.New("Fail to retrieve from DB").Error())
+			utils.JSON(w, http.StatusBadRequest, returnJson)
+			return
+		}
+	} else {
+		intId, err := strconv.Atoi(stringId)
+		if err != nil {
+			returnJson.Set("Status", false)
+			returnJson.Set("Error", errors.New("invalid SSH Connection id").Error())
+			utils.JSON(w, http.StatusBadRequest, returnJson)
+			return
+		}
+		PackageList, err = models.GetAllPackageFromHostID(intId)
+		if err != nil {
+			returnJson.Set("Status", false)
+			returnJson.Set("Error", errors.New("Fail to retrieve from DB").Error())
+			utils.JSON(w, http.StatusBadRequest, returnJson)
+			return
+		}
+	}
 
 	// Return json
 
