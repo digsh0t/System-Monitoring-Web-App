356d897e58f7abaeacb335a7d883d531b6ff5db5
longhk2802 <longhk2802@gmail.com>
2021-08-30 19:03:48 -0700

    update role for authorization

--- routes/hostUserAdd.go
+++ routes/hostUserAdd.go
@@ -15,7 +15,7 @@ import (
 func HostUserAdd(w http.ResponseWriter, r *http.Request) {
 
 	//Authorization
-	isAuthorized, err := auth.CheckAuth(r, []string{"admin", "user"})
+	isAuthorized, err := auth.CheckAuth(r, []string{"admin"})
 	if err != nil {
 		utils.ERROR(w, http.StatusUnauthorized, errors.New("invalid token").Error())
 		return
--- routes/hostUserRemove.go
+++ routes/hostUserRemove.go
@@ -15,7 +15,7 @@ import (
 func HostUserRemove(w http.ResponseWriter, r *http.Request) {
 
 	//Authorization
-	isAuthorized, err := auth.CheckAuth(r, []string{"admin", "user"})
+	isAuthorized, err := auth.CheckAuth(r, []string{"admin"})
 	if err != nil {
 		utils.ERROR(w, http.StatusUnauthorized, errors.New("invalid token").Error())
 		return
