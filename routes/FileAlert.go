382bdd78f456cd5bfaa855e9f0a1b90ad9089b1d
Tri Le <wintltr@gmail.com>
2021-09-04 20:13:42 +0700

    add authorization check to fileAlert.go

--- routes/FileAlert.go
+++ routes/FileAlert.go
@@ -2,14 +2,37 @@ package routes
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/wintltr/login-api/alerts"
+	"github.com/wintltr/login-api/auth"
 	"github.com/wintltr/login-api/utils"
 )
 
 func WatchFile(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	if r.Method == "OPTIONS" {
+		//CORS
+		// return "OKOK"
+		json.NewEncoder(w).Encode("OKOK")
+		return
+	}
+
+	//Authorization
+	isAuthorized, err := auth.CheckAuth(r, []string{"admin"})
+	if err != nil {
+		utils.ERROR(w, http.StatusUnauthorized, errors.New("invalid token").Error())
+		return
+	}
+	if !isAuthorized {
+		utils.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized").Error())
+		return
+	}
+
 	type file struct {
 		Filepath string `json:"filepath"`
 	}
