1d35981e73d0d05899f339c06321b63b6abc97b5
Tri Le <wintltr@gmail.com>
2021-09-27 23:27:56 +0700

    add uac model

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/febrianrendak/go-ansible v0.4.1
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1
@@ -15,12 +14,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/robfig/cron/v3 v3.0.1
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
 	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/validator.v2 v2.0.0-20210331031555-b37d688a7fb0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ models/windows_client_policy.go
@@ -0,0 +1,79 @@
+package models
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+type uacSetting struct {
+	Data string `json:"data"`
+	Path string `json:"path"`
+}
+
+func parseUAC(output string) ([]uacSetting, error) {
+	var uacList []uacSetting
+	err := json.Unmarshal([]byte(output), &uacList)
+	return uacList, err
+}
+
+func (sshConnection SshConnectionInfo) GetUACSettings() ([]uacSetting, error) {
+	var uacList []uacSetting
+	result, err := sshConnection.RunCommandFromSSHConnectionUseKeys(`osqueryi --json "SELECT data, path FROM registry WHERE key = 'HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Policies\System'"`)
+	if err != nil {
+		return uacList, err
+	}
+	uacList, err = parseUAC(result)
+	return uacList, err
+}
+
+func beautifyUACList(uacList []uacSetting) {
+
+	consentPromptBehaviorUser := map[string]string{
+		"0": "Automatically deny elevation requests.",
+		"1": "Prompt for credentials on the secure desktop.",
+		"3": "Prompt for credentials.",
+	}
+
+	consentPromptBehaviorAdmin := map[string]string{
+		"0": "Elevate without prompting.",
+		"1": "Prompt for credentials on the secure desktop.",
+		"2": "Prompt for consent on the secure desktop.",
+		"3": "Prompt for credentials.",
+		"4": "Prompt for consent.",
+		"5": "Prompt for consent for non-Windows binaries.",
+	}
+
+	dscAutomationHostEnabled := map[string]string{
+		"0": "Disable configuring the machine at boot-up.",
+		"1": "Enable configuring the machine at boot-up.",
+		"2": "Enable configuring the machine only if DSC is in pending or current state. This is the default value.",
+	}
+
+	uacPath := map[string]string{
+		"FilterAdministratorToken":   "Admin Approval Mode for the built-in Administrator account.",
+		"EnableUIADesktopToggle":     "Allow UIAccess applications to prompt for elevation without using the secure desktop.",
+		"ConsentPromptBehaviorAdmin": "Behavior of the elevation prompt for administrators in Admin Approval Mode.",
+		"ConsentPromptBehaviorUser":  "Behavior of the elevation prompt for standard users.",
+		"EnableInstallerDetection":   "Detect application installations and prompt for elevation.",
+		"ValidateAdminCodeSignatures	": "Only elevate executables that are signed and validated.",
+		"EnableSecureUIAPaths":  "Only elevate UIAccess applications that are installed in secure locations.",
+		"EnableLUA":             "Run all administrators in Admin Approval Mode.",
+		"PromptOnSecureDesktop": "Switch to the secure desktop when prompting for elevation.",
+		"EnableVirtualization":  "Virtualize file and registry write failures to per-user locations.",
+	}
+
+	for _, uacSetting := range uacList {
+		if strings.Contains(uacSetting.Path, "ConsentPromptBehaviorUser") {
+			uacSetting.Data = consentPromptBehaviorUser[uacSetting.Data]
+			uacSetting.Path = "Behavior of the elevation prompt for standard users"
+		}
+		if strings.Contains(uacSetting.Path, "ConsentPromptBehaviorAdmin") {
+			uacSetting.Data = consentPromptBehaviorAdmin[uacSetting.Data]
+			uacSetting.Path = "Behavior of the elevation prompt for administrators in Admin Approval Mode"
+		}
+		if strings.Contains(uacSetting.Path, "DSCAutomationHostEnabled") {
+			uacSetting.Data = dscAutomationHostEnabled[uacSetting.Data]
+			uacSetting.Path = "Behavior of the elevation prompt for administrators in Admin Approval Mode"
+		}
+	}
+}
