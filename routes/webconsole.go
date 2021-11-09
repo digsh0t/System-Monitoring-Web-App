938e3c7a85caa4303ddc2f5dab15b18422366a84
longhk2802 <longhk2802@gmail.com>
2021-11-09 21:13:30 +0700

    commit

---
+++ routes/webconsole.go
@@ -0,0 +1,83 @@
+package routes
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"github.com/wintltr/login-api/models"
+	"golang.org/x/crypto/ssh"
+)
+
+var (
+	//Allow cross-domain
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func WebConsoleWSHanlder(w http.ResponseWriter, r *http.Request) {
+	var (
+		conn    *websocket.Conn
+		client  *ssh.Client
+		sshConn *webconsole.SSHConnect
+		err     error
+	)
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		return
+	}
+	defer conn.Close()
+
+	sshConnection, err := models.GetSSHConnectionFromId(id)
+	if err != nil {
+		webconsole.WsSendText(conn, []byte(errors.New("SSH Connection id is not available").Error()))
+		return
+	}
+	//Create ssh client
+	if sshConnection.PasswordSSH == "" {
+		client, err = sshConnection.ConnectSSHWithSSHKeys()
+	} else {
+		client, err = sshConnection.ConnectSSHWithPassword()
+	}
+	if err != nil {
+		webconsole.WsSendText(conn, []byte(err.Error()))
+		return
+	}
+	defer client.Close()
+
+	//connect to ssh
+	if sshConn, err = webconsole.NewSSHConnect(client); err != nil {
+		webconsole.WsSendText(conn, []byte(err.Error()))
+		return
+	}
+
+	quit := make(chan int)
+	go sshConn.Output(conn, quit)
+	go sshConn.Recv(conn, quit)
+	<-quit
+}
+
+func WebConsoleTemplate(w http.ResponseWriter, r *http.Request) {
+
+	type sshID struct {
+		ID string
+	}
+
+	var sI sshID
+	vars := mux.Vars(r)
+	sI.ID = vars["id"]
+	temp, e := template.ParseFiles("./template/index.html")
+	if e != nil {
+		fmt.Println(e)
+	}
+	temp.Execute(w, sI)
+	return
+}
