02a7958d565b4c4bf87b4805cb8423d1a68423bb
Tri Le <wintltr@gmail.com>
2021-12-08 23:11:08 +0700

    check error check file and url using virustotal

--- models/virustotalScan.go
+++ models/virustotalScan.go
@@ -1,12 +1,17 @@
 package models
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/Jeffail/gabs"
 	vt "github.com/VirusTotal/vt-go"
@@ -29,6 +34,8 @@ type VTReport struct {
 }
 
 func ScanFile(filename string) (VTReport, error) {
+	var counter int = 0
+	url := "https://www.virustotal.com/api/v3/files"
 	var report VTReport
 	utils.EnvInit()
 	apiKey := os.Getenv("VT_API_TOKEN")
@@ -47,14 +54,115 @@ func ScanFile(filename string) (VTReport, error) {
 
 	fileSHA256 := fmt.Sprintf("%x", h.Sum(nil))
 
-	fileContent, err := os.Open("putty-64bit-0.76-installer.msi")
+	values := map[string]io.Reader{
+		"file": mustOpen(filename), // lets assume its this file
+	}
+	err = Upload(http.DefaultClient, url, values, f.Name())
 	if err != nil {
 		return report, err
 	}
-	_, err = client.NewFileScanner().Scan(fileContent, "putty-64bit-0.76-installer.msi", nil)
+
+	report, err = getFileVTReport(client, fileSHA256)
 	if err != nil {
 		return report, err
 	}
+	for report.AnalysisTime == "{}" {
+		report, err = getFileVTReport(client, fileSHA256)
+		if err != nil {
+			return report, err
+		}
+		if report.AnalysisTime == "{}" {
+			if counter == 1 {
+				return report, errors.New("file is in queue to scan, please try later")
+			}
+			time.Sleep(30 * time.Second)
+			counter++
+		}
+	}
+	return report, err
+}
+
+func Upload(client *http.Client, url string, values map[string]io.Reader, filename string) (err error) {
+	// Prepare a form that you will submit to that URL.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	for key, r := range values {
+		var fw io.Writer
+		if x, ok := r.(io.Closer); ok {
+			defer x.Close()
+		}
+		// Add an image file
+		if x, ok := r.(*os.File); ok {
+			if fw, err = w.CreateFormFile(key, x.Name()); err != nil {
+				return
+			}
+		} else {
+			// Add other fields
+			if fw, err = w.CreateFormField(key); err != nil {
+				return
+			}
+		}
+		if _, err = io.Copy(fw, r); err != nil {
+			return err
+		}
+
+	}
+	// Don't forget to close the multipart writer.
+	// If you don't close it, your request will be missing the terminating boundary.
+	w.Close()
+
+	// Now that you have a form, you can submit it to your handler.
+	contentType := getContentTypeFromFile(filename)
+	newBuffer := strings.ReplaceAll(b.String(), "Content-Type: application/octet-stream", "Content-Type: "+contentType)
+	//b.Bytes() = []byte(newBuffer)
+	req, err := http.NewRequest("POST", url, bytes.NewBufferString(newBuffer))
+	if err != nil {
+		return
+	}
+	// Don't forget to set the content type, this will contain the boundary.
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("x-apikey", os.Getenv("VT_API_TOKEN"))
+	req.Header.Add("Content-Type", "multipart/form-data")
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// Submit the request
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	// Check the response
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("bad status: %s", res.Status)
+	}
+	return
+}
+
+func getContentTypeFromFile(filename string) string {
+	var extension string
+	contentTypes := map[string]string{
+		"msi":   "application/x-msi",
+		"octet": "application/octet-stream",
+	}
+	tmp := strings.Split(filename, ".")
+	if tmp == nil {
+		extension = "octet"
+	} else {
+		extension = tmp[len(tmp)-1]
+	}
+	return contentTypes[extension]
+}
+
+func mustOpen(f string) *os.File {
+	r, err := os.Open(f)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+func getFileVTReport(client *vt.Client, fileSHA256 string) (VTReport, error) {
+	var report VTReport
 	file, err := client.GetObject(vt.URL("files/%s", fileSHA256))
 	if err != nil {
 		return report, err
@@ -77,6 +185,7 @@ func ScanFile(filename string) (VTReport, error) {
 
 func ScanURL(url string) (VTReport, error) {
 	var report VTReport
+	var counter int
 	utils.EnvInit()
 	apiKey := os.Getenv("VT_API_TOKEN")
 	hasher := sha256.New()
@@ -89,11 +198,33 @@ func ScanURL(url string) (VTReport, error) {
 		return report, err
 	}
 
-	vtURL, err := client.GetObject(vt.URL("urls/%s", hashedURL))
+	report, err = getURLVTReport(client, hashedURL)
 	if err != nil {
 		return report, err
 	}
-	out, err := json.Marshal(vtURL)
+	for report.AnalysisTime == "{}" {
+		report, err = getURLVTReport(client, hashedURL)
+		if err != nil {
+			return report, err
+		}
+		if report.AnalysisTime == "{}" {
+			if counter == 1 {
+				return report, errors.New("file is in queue to scan, please try later")
+			}
+			time.Sleep(30 * time.Second)
+			counter++
+		}
+	}
+	return report, err
+}
+
+func getURLVTReport(client *vt.Client, hashedURL string) (VTReport, error) {
+	var report VTReport
+	file, err := client.GetObject(vt.URL("urls/%s", hashedURL))
+	if err != nil {
+		return report, err
+	}
+	out, err := json.Marshal(file)
 	if err != nil {
 		return report, err
 	}
@@ -105,6 +236,6 @@ func ScanURL(url string) (VTReport, error) {
 	}
 	json.Unmarshal(jsonParsed.Path("attributes.last_analysis_stats").Bytes(), &report.Attributes)
 	report.AnalysisTime = jsonParsed.Path("attributes.last_analysis_date").String()
-	report.URL = strings.Trim(strings.ReplaceAll(jsonParsed.Path("links.self").String(), "https://www.virustotal.com/api/v3/urls", "https://www.virustotal.com/gui/url"), `"`)
+	report.URL = strings.Trim(strings.ReplaceAll(jsonParsed.Path("links.self").String(), "https://www.virustotal.com/api/v3/files", "https://www.virustotal.com/gui/file"), `"`)
 	return report, err
 }
